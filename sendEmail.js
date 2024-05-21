ccaa12c92b85d4a984fdfaa3b813c3eab822b7fe
bikram11git <bikram@myparticipants.com>
2024-05-21 13:53:26 +0530

    fixed the api

--- sendEmail.js
+++ sendEmail.js
@@ -7,19 +7,17 @@ const path = require('path');
 const KEYFILEPATH = path.join(__dirname, 'credentials.json');
 
 // Read and parse the credentials JSON file
-const credentials = JSON.parse(fs.readFileSync(KEYFILEPATH));
+const credentials = JSON.parse(fs.readFileSync(KEYFILEPATH, 'utf-8'));
 
-// Configure Nodemailer to generate an SMTP configuration
 const mailTransport = nodemailer.createTransport({
   streamTransport: true,
   newline: 'unix',
   buffer: true
 });
 
-// Email contents
 const mailOptions = {
   from: 'bikram@myparticipants.com',
-  to: ['bikram.deep.yadav.11@gmail.com'],
+  to: ['b422020@iiit-bh.ac.in'],
   subject: 'Test Email via Gmail API',
   text: 'Hello! This is a test email sent via Gmail API with a service account.',
   html: '<p>Hello. This is using <strong>Gmail API</strong></p>'
@@ -27,42 +25,41 @@ const mailOptions = {
 
 // Function to send the email using Gmail API
 async function sendMimeMessage(mimeMessage) {
-  const gmail = google.gmail({ version: 'v1' });
-  
-  const jwtClient = new google.auth.JWT(
-    // credentials.client_email,
-    // null,
-    // credentials.private_key.replace(/\\n/g, '\n'), // Replace escaped newlines
-    // ['https://www.googleapis.com/auth/gmail.send'],
-    // 'bikram@myparticipants.com' // Email address of the user the service account is impersonating
-   { keyFile : KEYFILEPATH,
-    scopes : ['https://www.googleapis.com/auth/gmail.send'],
-    subject: "bikram@myparticipants.com"
-  }
-  );
-
-  // Authorize the JWT client and get a token to make API calls
-  await jwtClient.authorize();
-
-  // Send the email using the Gmail API
-  const response = await gmail.users.messages.send({
-    auth: jwtClient,
-    userId: 'me',
-    resource: { raw: mimeMessage }
+  const jwtClient = new google.auth.JWT({
+    email: credentials.client_email,
+    key: credentials.private_key.replace(/\\n/g, '\n'), // Replace escaped newlines
+    scopes: ['https://www.googleapis.com/auth/gmail.send'],
+    subject: 'bikram@myparticipants.com'
   });
 
-  console.log('Email sent:', response.data.toString);
+  try {
+    // Authorize the JWT client and get a token to make API calls
+    await jwtClient.authorize();
+
+    const gmail = google.gmail({ version: 'v1', auth: jwtClient });
+
+    // Send the email using the Gmail API
+    const response = await gmail.users.messages.send({
+      userId: 'me',
+      requestBody: { raw: mimeMessage }
+    });
+
+    console.log('Email sent:', response.data);
+  } catch (error) {
+    if (error.response && error.response.data) {
+      console.error('Error details:', error.response.data);
+    }
+    throw error;
+  }
 }
 
-
-// Generate MIME message and send email
+// Generating the MIME message from buffer
 mailTransport.sendMail(mailOptions, (err, info) => {
   if (err) {
     return console.error('Failed to send mail:', err);
   }
-  const mimeMessage = Buffer.from(info.message.toString('base64')).toString('base64');
+  const mimeMessage = Buffer.from(info.message.toString(), 'utf-8').toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
   sendMimeMessage(mimeMessage)
     .then(() => console.log('Email sent successfully.'))
     .catch(error => console.error('Error sending email:', error));
 });
-
